008857a6e64df0314ad35678b182dca24eb52981
azielinski <aron.zielinski@altkom.pl>
2017-01-30 16:25:52 +0100

    initial commit

---
+++ src/main/java/library/Main.java
@@ -0,0 +1,12 @@
+package library;
+
+/**
+ * @author azielinski1
+ */
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("Hello fuckers!!!");
+
+    }
+}
